a23a7eea1511710d8a9cebf68974cf2276ea85c8
Ahmet Aslan <simsar6155@gmail.com>
2022-07-01 10:10:15 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+    class Logging:ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+    internal interface ILogging
+    {
+        void Log();
+    }
+    class Caching:ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached");
+        }
+    }
+
+    internal interface ICaching
+    {
+        void Cache();
+    }
+    class Validation : IValidate
+    {
+        public void Validate()
+        {
+            Console.WriteLine("Validated");
+        }
+    }
+
+    internal interface IValidate
+    {
+        void Validate();
+    }
+
+    class Authorize:IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("User Checked");
+        }
+    }
+
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttongConsernsFacade _concerns;
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttongConsernsFacade();
+        }
+        public void Save()
+        {
+            _concerns.Logging.Log();
+            _concerns.Caching.Cache();
+            _concerns.Authorize.CheckUser();
+            _concerns.Validate.Validate();
+            Console.WriteLine("Saved");
+        }
+    }
+    class CrossCuttongConsernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+        public IValidate Validate;
+
+        public CrossCuttongConsernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
+            Validate = new Validation();
+        }
+    }
+}
